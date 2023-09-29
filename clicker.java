c9a892f8aedcf8e160911983bd5a262a77594237
ETesseract <etesseract@icloud.com>
2023-09-29 14:06:59 -0600

    Hello, world!

--- clicker.java
+++ clicker.java
@@ -1,5 +1,6 @@
 public class clicker {
     final int MAXCPS = 10; //Fun fact: The world record is 105.1 CPS
     public static void main(String[] args) {
+        System.out.println("Hello, world!");
     }
 }
