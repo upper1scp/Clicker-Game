e2389117cc0cdf344b1342e49154ec51b49d33e5
ETesseract <etesseract@icloud.com>
2023-09-29 14:06:00 -0600

    Fixed main class (Shouldn't catch on fire anymore)

--- Main.java
+++
@@ -1,3 +0,0 @@
-public class Main {
-    public final int MAXCPS = 10; //Fun fact: The world record is 105.1 CPS
-}
---
+++ clicker.java
@@ -0,0 +1,5 @@
+public class clicker {
+    final int MAXCPS = 10; //Fun fact: The world record is 105.1 CPS
+    public static void main(String[] args) {
+    }
+}
